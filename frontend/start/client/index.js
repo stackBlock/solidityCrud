d4b280d4bd98e571cbb27b20ef77d21e5e1b9b0a
stackblock <stackchain@gmail.com>
2020-06-05 19:23:22 -0400

    Two functions attached to html (create, read).

--- frontend/start/client/index.js
+++ frontend/start/client/index.js
@@ -1,52 +1,88 @@
-import Web3 from 'web3';
-import Crud from '../build/contracts/Crud.json';
+import Web3 from "web3"; // import web3
+import Crud from "../build/contracts/Crud.json"; // import contract artifact
 
-let web3;
-let crud;
+let web3; // web3 instance variable
+let crud; // contract instance variable
 
 const initWeb3 = () => {
+  // initialize web3
   return new Promise((resolve, reject) => {
-    if(typeof window.ethereum !== 'undefined') {
-      const web3 = new Web3(window.ethereum);
-      window.ethereum.enable()
+    if (typeof window.ethereum !== "undefined") {
+      // check if metamask has injected itself into the page
+      const web3 = new Web3(window.ethereum); // initiate web3
+      window.ethereum
+        .enable() // prompt user to accept our apps
         .then(() => {
-          resolve(
-            new Web3(window.ethereum)
-          );
+          resolve(new Web3(window.ethereum));
         })
-        .catch(e => {
+        .catch((e) => {
           reject(e);
         });
       return;
     }
-    if(typeof window.web3 !== 'undefined') {
+    if (typeof window.web3 !== "undefined") {
+      // check if it is an old metamask version
       return resolve(
-        new Web3(window.web3.currentProvider)
+        new Web3(window.web3.currentProvider) // do the same stuff
       );
     }
-    resolve(new Web3('http://localhost:9545'));
+    resolve(new Web3("http://localhost:9545")); // use local host with ganache
   });
 };
 
 const initContract = () => {
-  const deploymentKey = Object.keys(Crud.networks)[0];
-  return new web3.eth.Contract(
-    Crud.abi, 
-    Crud
-      .networks[deploymentKey]
-      .address
-  );
+  // initialize contract instance
+  const deploymentKey = Object.keys(Crud.networks)[0]; // dynamically get the address
+  return new web3.eth.Contract(Crud.abi, Crud.networks[deploymentKey].address);
 };
 
 const initApp = () => {
+  const $create = document.getElementById("create");
+  const $createResult = document.getElementById("create-result");
+  const $read = document.getElementById("read");
+  const $readResult = document.getElementById("read-result");
+  let accounts = [];
+
+  web3.eth.getAccounts().then((_account) => {
+    accounts = _accounts;
+  });
+
+  $create.addEventListener("submit", (e) => {
+    e.preventDefault;
+    const name = e.target.elements[0].value;
+    crud.methods
+      .create(name)
+      .send({ from: account[0] })
+      .then(() => {
+        $createResult.innerHTML = `New user ${name} was successfully created!`;
+      })
+      .catch(() => {
+        $createResult.innerHTML = `oops... there was an error while trying to create a new user...`;
+      });
+  });
+
+  $read.addEventListener("submit", (e) => {
+    e.preventDefault;
+    const id = e.target.elements[0].value;
+    crud.methods
+      .read(id)
+      .call()
+      .then((result) => {
+        $readResult.innerHTML = `The user id ${result[0]} the user name is ${result[1]}`;
+      })
+      .catch(() => {
+        $createResult.innerHTML = `oops... there was an error`;
+      });
+  });
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  initWeb3()
-    .then(_web3 => {
-      web3 = _web3;
-      crud = initContract();
-      initApp(); 
+document.addEventListener("DOMContentLoaded", () => {
+  // waiting for html page to load
+  initWeb3() // initialize web3
+    .then((_web3) => {
+      web3 = _web3; // save web3 in a global variable
+      crud = initContract(); // initialize the contract to get the instance
+      initApp(); // trigger initApp where everything is set up
     })
-    .catch(e => console.log(e.message));
+    .catch((e) => console.log(e.message));
 });
