67cd5bb6f78648be444b66861d7910505b8efb7c
stackblock <stackchain@gmail.com>
2020-06-05 12:26:53 -0400

    Testing finished and correct!

--- tests/start/test/crud.js
+++ tests/start/test/crud.js
@@ -1,6 +1,53 @@
-const Crud = artifacts.require('Crud');
+const Crud = artifacts.require("Crud");
 
-contract('Crud', () => {
+contract("Crud", () => {
+  let crud = null;
+  before(async () => {
+    crud = await Crud.deployed();
+  });
+
+  it("Should create a new user", async () => {
+    await crud.create("Frank");
+    const user = await crud.read(1);
+    assert(user[0].toNumber() === 1);
+    assert(user[1] === "Frank");
+  });
+
+  it('Should update a user', async () => {
+      await crud.update(1, 'Frankk');
+      const user = await crud.read(1);
+      assert(user[0].toNumber() === 1);
+      assert(user[1] === 'Frankk');
+  });
+
+  it('Should not update a non-existing user', async () => {
+      try {
+          await crud.update(2, 'Franky');
+      } catch(e) {
+          assert(e.message.includes('User does not exist!'));
+          return;
+      }
+      asssert(false);
+  })
+
+  it('Should destroy (delete) as user', async () => {
+      await crud.destroy(1);
+      try {
+          await crud.read(1);
+      } catch(e) {
+          assert(e.message.includes('User does not exist!'));
+          return;
+      }
+      assert(false);
+  })
+
+  it('Should not destroy a non-existing user', async () => {
+      try {
+          await crud.destroy(10);
+      } catch(e) {
+          assert(e.message.includes('User does not exist!'));
+          return;
+      }
+      assert(false);
+  })
 });
-
-
